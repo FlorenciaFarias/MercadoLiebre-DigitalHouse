3972bac79bc821068cb6978a383506ba4a796913
Flor Farías <florencia.fariasmm@gmail.com>
2021-11-23 11:00:59 -0300

    procfile

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const app= express ();
 
 app.use(express.static('public'));
 
-app.listen(3030, () => {
-    console.log('Servidor corriendo en el puerto 3030');
+app.listen (process.env.PORT ||3000, ()=>{
+    console.log('Servidor funcionando bien');
 });
 
-console.log(__dirname);
 
 app.get('/', (req, res) => {
 res.sendFile(__dirname + '/views/home.html')
